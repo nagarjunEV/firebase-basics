5d62f2019420bd29b3ddc6823ad32f7e4956a2b8
nagarjunEV <nagarjunev.11@gmail.com>
2022-01-02 11:11:17 -0600

    auth added

--- public/app.js
+++ public/app.js
@@ -1,2 +1,34 @@
+
+
 //global variable
 console.log(firebase)
+
+const auth = firebase.auth();
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+const userDetails = document.getElementById('userDetails');
+const whenSignedOut = document.getElementById('whenSignedOut');
+const whenSignedIn = document.getElementById('whenSignedIn');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => {
+    auth.signInWithPopup(provider);
+}
+
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}! with User UID - ${user.uid}</h3>`;
+    } else {
+        // signed out
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
